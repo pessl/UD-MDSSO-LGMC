8d47ec4b1236b55c0ba145874e9af700e423445d
Pedro Soria <psorialopez84@gmail.com>
2024-09-15 16:41:56 -0500

    Actualización en la versión del JJWT

--- src/main/java/com/cursos/api/springsecuritycourse/service/auth/JwtService.java
+++ src/main/java/com/cursos/api/springsecuritycourse/service/auth/JwtService.java
@@ -1,16 +1,14 @@
 package com.cursos.api.springsecuritycourse.service.auth;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
 
@@ -28,19 +26,41 @@ public class JwtService {
         Date issuedAt = new Date(System.currentTimeMillis());
         Date expiration = new Date((EXPIRATION_IN_MINUTES * 60 * 1000) + issuedAt.getTime());
 
-        String jwt = Jwts.builder()
+        // DEPRECATED
+        /*String jwt = Jwts.builder()
                 .setClaims(extraClaims)
                 .setSubject(user.getUsername())
                 .setIssuedAt(issuedAt)
                 .setExpiration(expiration)
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .compact();*/
+
+        String jwt = Jwts.builder()
+                .header()
+                    .type("JWT")
+                    .and()
+
+                .subject(user.getUsername())
+                .issuedAt(issuedAt)
+                .expiration(expiration)
+                .claims(extraClaims)
+
+                .signWith(generateKey(), Jwts.SIG.HS256)
+
                 .compact();
 
         return jwt;
     }
 
-    private Key generateKey() {
+    // DEPRECATED
+    /*private Key generateKey() {
+        byte[] passwordDecoded = Decoders.BASE64.decode(SECRET_KEY);
+        System.out.println(new String(passwordDecoded));
+        return Keys.hmacShaKeyFor(passwordDecoded);
+    }*/
+
+    private SecretKey generateKey() {
         byte[] passwordDecoded = Decoders.BASE64.decode(SECRET_KEY);
         System.out.println(new String(passwordDecoded));
         return Keys.hmacShaKeyFor(passwordDecoded);
@@ -50,8 +70,12 @@ public class JwtService {
         return extractAllClaims(jwt).getSubject();
     }
 
+    // DEPRECATED
+    /*private Claims extractAllClaims(String jwt) {
+        return Jwts.parserBuilder().setSigningKey(generateKey()).build().parseClaimsJws(jwt).getBody();
+    }*/
+
     private Claims extractAllClaims(String jwt) {
-       return Jwts.parserBuilder().setSigningKey(generateKey()).build()
-               .parseClaimsJws(jwt).getBody();
+        return Jwts.parser().verifyWith(generateKey()).build().parseSignedClaims(jwt).getPayload();
     }
 }
