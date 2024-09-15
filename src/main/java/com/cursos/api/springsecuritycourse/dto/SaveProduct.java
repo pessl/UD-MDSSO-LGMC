08a4f70bc39e0a0438be6ceae9b92e2a7ff19441
Pedro Soria <psorialopez84@gmail.com>
2024-09-14 22:26:49 -0500

    Poblando las tablas y manejo de excepciones

--- src/main/java/com/cursos/api/springsecuritycourse/dto/SaveProduct.java
+++ src/main/java/com/cursos/api/springsecuritycourse/dto/SaveProduct.java
@@ -11,7 +11,7 @@ public class SaveProduct implements Serializable {
     @NotBlank
     private String name;
 
-    @DecimalMin(value = "0.01")
+    @DecimalMin(value = "0.01", message = "El precio debe ser mayor a cero.")
     private BigDecimal price;
 
     @Min(value = 1)
